eac4a43aa8deab62e2d18082612209181aabfdb7
Ben Ayed Mehdi <mehdibenayed97@gmail.com>
2022-10-31 02:24:57 +0100

    Add reports controller

---
+++ FindYourWayAPI/Controllers/ReportsController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using FindYourWayAPI.Data;
+using FindYourWayAPI.Models;
+using FindYourWayAPI.Models.DAO;
+
+namespace FindYourWayAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportsController : ControllerBase
+    {
+        private readonly FindYourWayDbContext _context;
+
+        public ReportsController(FindYourWayDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Reports
+        /// <summary>
+        /// Get all reports of a specific company
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        public async Task<ActionResult<IEnumerable<Report>>> GetReports(int id)
+        {
+            return await _context.Reports.Where(r=>r.CompanyId==id).ToListAsync();
+        }
+
+        // GET: api/Reports/5
+        /// <summary>
+        /// Get a report by ID
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("report/{id}")]
+        public async Task<ActionResult<Report>> GetReport(int id)
+        {
+            var report = await _context.Reports.FindAsync(id);
+
+            if (report == null)
+            {
+                return NotFound();
+            }
+
+            return report;
+        }
+
+        // PUT: api/Reports/5
+        /// <summary>
+        /// Update an existing report
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="report"></param>
+        /// <returns></returns>
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutReport(int id, Report report)
+        {
+            if (id != report.ReportId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(report).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ReportExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Reports
+        /// <summary>
+        /// Add a new report
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<ActionResult<Report>> PostReport(AddReportRequest request)
+        {
+            var report = new Report
+            {
+                CompanyId = request.CompanyId,
+                ReportTitle = request.ReportTitle,
+                Content = request.Content,
+                IssuedAt = DateTime.Now
+            };
+            _context.Reports.Add(report);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetReport", new { id = report.ReportId }, report);
+        }
+
+        // DELETE: api/Reports/5
+        /// <summary>
+        /// Delete a report by ID
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteReport(int id)
+        {
+            var report = await _context.Reports.FindAsync(id);
+            if (report == null)
+            {
+                return NotFound();
+            }
+
+            _context.Reports.Remove(report);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ReportExists(int id)
+        {
+            return _context.Reports.Any(e => e.ReportId == id);
+        }
+    }
+}
---
+++ FindYourWayAPI/Models/DAO/AddReportRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace FindYourWayAPI.Models.DAO
+{
+    public class AddReportRequest
+    {
+        public string ReportTitle { get; set; }
+        public string Content { get; set; }
+        public int CompanyId { get; set; }
+    }
+}
